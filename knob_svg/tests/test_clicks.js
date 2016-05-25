1fb2a7b06fae9e1d2a2cfc474dd59b4e32a1de7e
Henryk Paluch <hpaluch@seznam.cz>
2016-05-25 11:07:37 +0200

    Real single angle test

--- knob_svg/tests/test_clicks.js
+++ knob_svg/tests/test_clicks.js
@@ -7,7 +7,16 @@ casper.test.on("fail",function(failure){
 	casper.exit(1);
 });
 
-casper.test.begin('KnobSVG', 4, function suite(test) {
+/*
+// uncomment this if you want to see browser console.log output...
+// see: http://stackoverflow.com/questions/11864373/console-log-doesnt-work-in-casperjs-evaluate-with-settimout
+casper.on('remote.message', function(msg) {
+    casper.echo('Remote CONSOLE: ' + msg);
+})
+*/
+
+
+casper.test.begin('KnobSVG', 5, function suite(test) {
 
 	casper.start('http://localhost/html5/knob_svg/',function(){
 
@@ -23,6 +32,44 @@ casper.test.begin('KnobSVG', 4, function suite(test) {
 
 	casper.waitForText(text0,function(){
 		test.assertSelectorHasText('#value1',text0);
+	});
+
+	var xCenter = 150/2;
+	var yCenter = 100/2;
+
+	var angleDeg = 45;
+	var angle    = angleDeg * Math.PI / 180;
+	var x = 42 * Math.cos( angle ) + xCenter;
+	var y = -42 * Math.sin( angle ) + yCenter;
+
+/*
+	casper.repeat(360/45, function (){
+
+
+	}
+*/
+
+	casper.then(function() {
+		// casper.echo("x: "+x+", y: "+y);
+		casper.click('#svg1',x,y);
+	});
+
+	casper.waitFor(function(){
+
+		// this executes in browser
+		return casper.evaluate( function( angleDeg,angle ){
+			// our page includes jQuery, so...
+			var value1 = $('#value1').text();
+			// console.log("value1: "+value1);
+			var items = value1.split(/\s+/);
+			var gotRad = items[1];
+			var gotDeg = items[3];
+			// avoid some runding error (2 Deg, 0.02 rad)
+			return  Math.abs( gotDeg - angleDeg ) < 2 
+				&& Math.abs( gotRad - angle ) < 0.02;
+		},angleDeg, angle);
+	}, function then(){
+		test.pass("Check of angle test for Deg = "+angleDeg);
 	});
 
 	casper.run(function() {
